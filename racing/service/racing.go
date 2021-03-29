2abd5352a4ef45facfd559645c3b75d6d30d5223
Narendra Mongiya <narendramongiya@Narendras-Air.hub>
2021-03-29 20:18:43 +1000

    added rpc

--- racing/service/racing.go
+++ racing/service/racing.go
@@ -1,14 +1,15 @@
 package service
 
 import (
-	"git.neds.sh/matty/entain/racing/db"
-	"git.neds.sh/matty/entain/racing/proto/racing"
+	"entain-master/racing/db"
+	"entain-master/racing/proto/racing"
 	"golang.org/x/net/context"
 )
 
 type Racing interface {
 	// ListRaces will return a collection of races.
 	ListRaces(ctx context.Context, in *racing.ListRacesRequest) (*racing.ListRacesResponse, error)
+	GetRace(ctx context.Context, in *racing.GetRaceRequest) (*racing.ListRacesResponse, error)
 }
 
 // racingService implements the Racing interface.
@@ -19,6 +20,16 @@ type racingService struct {
 // NewRacingService instantiates and returns a new racingService.
 func NewRacingService(racesRepo db.RacesRepo) Racing {
 	return &racingService{racesRepo}
+}
+
+func (s *racingService) GetRace(ctx context.Context, in *racing.GetRaceRequest) (*racing.ListRacesResponse, error) {
+    races, err := s.racesRepo.GetRace(in.GetId())
+	if err != nil {
+		return nil, err
+	}
+
+	return &racing.ListRacesResponse{Races: races}, nil
+
 }
 
 func (s *racingService) ListRaces(ctx context.Context, in *racing.ListRacesRequest) (*racing.ListRacesResponse, error) {
